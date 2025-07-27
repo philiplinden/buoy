c4b0ddb8ce7b496221858da75da826efeb4ac09d
Philip Linden <lindenphilipj@gmail.com>
2025-07-27 17:06:58 -0400

    dev: remove the repl for now

--- crates/buoy-runtime/bin/main.rs
+++ crates/buoy-runtime/bin/main.rs
@@ -2,13 +2,13 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
+use buoy_runtime::objects::balloon::{Balloon, BalloonConfig};
 
 fn main() {
     let mut app = App::new();
 
     app.add_plugins((
         buoy_runtime::BuoyDefaultPlugins,
-        buoy_runtime::ConsolePlugin,
         buoy_physics::BuoyPhysicsPlugin,
         bevy_common_assets::ron::RonAssetPlugin::<buoy_physics::ideal_gas::GasPropertiesConfig>::new(&["configs/properties.ron"]),
     ));
@@ -18,5 +18,31 @@ fn main() {
     #[cfg(feature = "debug-ui")]
     app.add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::new());
 
+    app.add_systems(PostStartup, setup_scenario);
+    app.add_systems(Update, log_balloon_position);
     app.run();
+}
+
+fn setup_scenario(
+    mut commands: Commands,
+) {
+    commands.spawn((
+        Balloon::new_from_config(
+            &BalloonConfig {
+                balloon_size: 1.0,
+                lift_gas_species: "helium".to_string(),
+                lift_gas_mass: 10.0,
+                balloon_mass: 1.0,
+                payload_mass: 1.0,
+            },
+        ),
+    ));
+}
+
+fn log_balloon_position(
+    balloon: Query<&Transform, (With<Balloon>, Changed<Transform>)>,
+) {
+    for transform in balloon.iter() {
+        info!("{:?}", transform.translation);
+    }
 }
